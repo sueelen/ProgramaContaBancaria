5fc304e904a7e2949f74efdd38da47729664b220
Sueelen <sueelenhornung@gmail.com>
2022-03-15 18:20:05 -0300

    concluindo o basico da classe Conta

--- src/aplication/Program.java
+++ src/aplication/Program.java
@@ -1,10 +1,19 @@
 package aplication;
 
+import java.util.Locale;
+import java.util.Scanner;
+
 public class Program {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner (System.in);
+		
+		
+		
+		
+		sc.close();
+		
 	}
 
 }
---
+++ src/entities/Conta.java
@@ -0,0 +1,43 @@
+package entities;
+
+public class Conta {
+	
+	private int numero;
+	private String titular;
+	private double saldo;
+	
+	public Conta(int numero, String titular) {
+		this.numero = numero;
+		this.titular = titular;
+	}
+	
+	public Conta(int numero, String titular, double depositoInicial) {
+		this.numero = numero;
+		this.titular = titular;
+		deposito(depositoInicial);
+		}
+
+	public int getNumero() {
+		return numero;
+	}
+	public String getTitular() {
+		return titular;
+	}
+	public void setTitular(String titular) {
+		this.titular = titular;
+	}
+	public double getSaldo() {
+		return saldo;
+	}
+	
+	public void deposito(double quantidadeDeDinheiro) {
+		saldo += quantidadeDeDinheiro;
+	}
+	
+	public void sacarDinheiro(double quantidadeDeDinheiro) {
+		saldo -= quantidadeDeDinheiro + 5.0;
+	}
+	
+	
+	
+}
